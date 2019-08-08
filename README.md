451f59733ad0bccd6b7307fe0a4b421e775696f3
Roman Rojas <32991189+Rrojaski@users.noreply.github.com>
2019-08-08 18:19:31 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Portfolio-Final
-Created with CodeSandbox
+<h1>To Do</h1>
+<ul>
+  <li>Implement email API for contact from</li>
+  <li>Update avatar and portrait images</li>
+  <li>Update text content</li>  
+  <li>Uptomize contact page for tablet view; min-height to full viewpoint</li>
+  <li>Finish Header; Remove header if neaded</li>
+</ul>
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./layout/Header/Header";
+import Footer from "./layout/Footer/Footer";
+import "reset.css";
+import "./styles.css";
+import Content from "./layout/Content/Content";
+import { HashRouter } from "react-router-dom";
+
+//removed Header
+function App() {
+  return (
+    <React.Fragment>
+      <HashRouter>
+        <Content />
+        <Footer />
+      </HashRouter>
+    </React.Fragment>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
